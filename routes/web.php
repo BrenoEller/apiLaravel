ff5796f4a37edad67ce72d77abcfff78508b2e87
Breno <brenoeller@hotmail.com>
2023-06-26 09:42:03 -0300

    new - organização de arquivos

--- routes/web.php
+++ routes/web.php
@@ -11,22 +11,14 @@ use App\Http\Controllers\ValorDeixouDeReceberController;
 // Agrupado por remetente
 Route::get('/agrupamento-remetente', [AgrupamentoRemetenteController::class, 'agrupamentoRemetente']);
 
-
 // Pesquisa valor total das notas referentes ao remetente
 Route::get('/valor-total-notas', [ValorTotalController::class, 'valorTotal']);
-
-
 
 // Pesquisa valor total de entregas já realizadas
 Route::get('/valor-total-entregue', [ValorTotalEntregueController::class, 'valorTotalEntregue']);
 
-
-
 // Notas atrasadas
 Route::get('/valor-notas-atrasadas', [ValorNotasAtrasadasController::class, 'valorNotasAtrasadas']);
-
-
-
 
 // Valor que o remetente deixou de receber por entregas atrasadas
 Route::get('/valor-deixou-de-receber', [ValorDeixouDeReceberController::class, 'deixouDeReceber']);
