b9be1fadd3a8058ceaa483cb90c939430b96eb1c
Breno <brenoeller@hotmail.com>
2023-06-26 12:25:16 -0300

    fix - valor deixou de receber

--- app/Http/Controllers/ValorDeixouDeReceberController.php
+++ app/Http/Controllers/ValorDeixouDeReceberController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use DateTime;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class ValorDeixouDeReceberController extends Controller
 {
@@ -17,27 +20,42 @@ class ValorDeixouDeReceberController extends Controller
 
         $resultado = [];
         $remetenteDesconhecido = [
-            'deixou_de_receber' => 0,
-            'quantidade_nao_entregue' => 0,
+            'valor_deixou_receber' => 0,
+            'quantidade_entregas' => 0,
         ];
-
         foreach ($apiArray as $item) {
+
             if ($cnpj && $item['cnpj_remete'] !== $cnpj) {
                 continue;
             }
 
-            if (isset($item['dt_entrega'])) {
-                continue;
+
+            if (isset($item['dt_emis']) && isset($item['dt_entrega'])) {
+
+                $dataEmissao = DateTime::createFromFormat('d/m/Y H:i:s', $item['dt_emis'])->setTime(0, 0, 0);
+                $dataEntrega = DateTime::createFromFormat('d/m/Y H:i:s', $item['dt_entrega'])->setTime(0, 0, 0);
+
+                $diff = $dataEntrega->diff($dataEmissao)->days;
+
+
+                if ($dataEmissao && $dataEntrega) {
+
+                    if ($diff > 2) {
+                        $remetente = $item['nome_remete'] ?? 'Remetente Desconhecido';
+
+                        if (!isset($resultado[$remetente])) {
+                            $resultado[$remetente] = $remetenteDesconhecido;
+                        }
+
+                        $resultado[$remetente]['valor_deixou_receber'] += floatval($item['valor']);
+                        $resultado[$remetente]['quantidade_entregas']++;
+                    }
+                } else {
+                    Log::error('Falha ao interpretar as datas:', ['item' => $item]);
+                }
+            } else {
+                Log::warning('Item de API ausente ou incompleto:', ['item' => $item]);
             }
-
-            $remetente = $item['nome_remete'] ?? 'Remetente Desconhecido';
-
-            if (!isset($resultado[$remetente])) {
-                $resultado[$remetente] = $remetenteDesconhecido;
-            }
-
-            $resultado[$remetente]['deixou_de_receber'] += floatval($item['valor']);
-            $resultado[$remetente]['quantidade_nao_entregue']++;
         }
 
         if (empty($resultado) && $cnpj) {
